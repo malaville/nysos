9abb479278232ec1b7256d4d2546591b274b49e8
Marc-Antoine Laville <marc-antoine@laville.work>
2020-10-14 19:35:37 +0200

    Added some buttons on each documents

--- nysos-frontend/src/app/app.module.ts
+++ nysos-frontend/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { DocumentViewerComponent } from './interface/document-viewer/document-viewer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+// Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DocumentViewerComponent } from './interface/document-viewer/document-viewer.component';
 import { SourceManagerComponent } from './interface/source-manager/source-manager.component';
+
 @NgModule({
   declarations: [AppComponent, DocumentViewerComponent, SourceManagerComponent],
   imports: [
@@ -23,6 +26,7 @@ import { SourceManagerComponent } from './interface/source-manager/source-manage
     FormsModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
