af28357850fe998a60da8e541dee5bac87adc611
Marc-Antoine Laville <marc-antoine@laville.work>
2020-10-15 21:37:59 +0200

    Better title in source-manager

--- nysos-frontend/src/app/interface/source-manager/source-manager.component.ts
+++ nysos-frontend/src/app/interface/source-manager/source-manager.component.ts
@@ -1,7 +1,8 @@
 import { Input, OnChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { filter, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AppstateService } from 'src/app/services/app/appstate.service';
 import { CytostateService } from 'src/app/services/cytostate/cytostate.service';
 import { BibliographyItem } from './bibliography-item';
@@ -11,14 +12,32 @@ import { BibliographyItem } from './bibliography-item';
   templateUrl: './source-manager.component.html',
   styleUrls: ['./source-manager.component.css'],
 })
-export class SourceManagerComponent implements OnChanges {
+export class SourceManagerComponent implements OnChanges, OnInit {
   myForm: FormGroup;
   public myreg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
   @Input() bibliographyId: string;
 
-  constructor(private fb: FormBuilder, private cytostate: CytostateService) {
+  title: Observable<string>;
+
+  constructor(
+    private fb: FormBuilder,
+    private cytostate: CytostateService,
+    private appState: AppstateService
+  ) {
     this.myForm = this.fb.group(new BibliographyItem().toFormGroupObject());
+  }
+
+  ngOnInit() {
+    this.title = this.appState.UIstateObservable.pipe(
+      map((uistate) =>
+        uistate.addingDocument
+          ? 'Add a new source to this theme'
+          : uistate.editDocument
+          ? 'Edit this source infos'
+          : ''
+      )
+    );
   }
 
   ngOnChanges(): void {
