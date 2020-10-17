8c393c4eab01c5b80c99b9005f5ca956a0391a91
Marc-Antoine Laville <marc-antoine@laville.work>
2020-10-17 10:02:40 +0200

    Debouncing SaveContent

--- nysos-frontend/src/app/interface/document-viewer/document-viewer.component.ts
+++ nysos-frontend/src/app/interface/document-viewer/document-viewer.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 import {
   AppstateService,
   DocumentDataStateInterface,
@@ -16,6 +16,10 @@ import { BibliographyItemLink } from '../source-manager/bibliography-item';
 export class DocumentViewerComponent {
   public bibliography: Observable<BibliographyItemLink[]> = of([]);
   documentStateObs: Observable<DocumentDataStateInterface>;
+  contentBS = new BehaviorSubject<{ contentId: string; content: string }>({
+    contentId: undefined,
+    content: '',
+  });
 
   @Input() large: boolean;
 
@@ -29,10 +33,20 @@ export class DocumentViewerComponent {
     this.bibliography = this.documentStateObs.pipe(
       map((doc) => this.cytostate.findBibliographyAbout(doc.contentId) || [])
     );
+    this.contentBS
+      .asObservable()
+      .pipe(debounceTime(4000))
+      .subscribe((content) => {
+        this.appState.saveContentOf(content.contentId, content.content);
+      });
   }
 
   onDescriptionChange(content) {
-    this.appState.saveContent(content);
+    this.contentBS.next({
+      contentId: this.appState.documentState.contentId,
+      content,
+    });
+    // this.appState.saveContent(content);
   }
 
   onLinkDescriptionChangeCallback = () => (id: string, description: string) =>
