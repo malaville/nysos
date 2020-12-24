0769fdd673bdd4f2338e27b7f883b4b42ce34033
Marc-Antoine Laville <marc-antoine@laville.work>
2020-12-24 18:07:59 +0100

    no warning on cytoscape

--- nysos-frontend/src/app/services/cytostate/cytoscape.injection.token.ts
+++ nysos-frontend/src/app/services/cytostate/cytoscape.injection.token.ts
@@ -1,18 +1,15 @@
 import { InjectionToken } from '@angular/core';
-import cytoscape from 'cytoscape';
-const edgehandles = require('cytoscape-edgehandles');
+import cytoscape, { Ext } from 'cytoscape';
+import edgehandles from 'cytoscape-edgehandles';
 
 export type Cytoscape = typeof cytoscape;
 
 export const CYTOSCAPE = new InjectionToken<Cytoscape>('Browser Storage', {
   providedIn: 'root',
   factory: () => {
-    console.log('Cytoscape gets created');
     try {
-      cytoscape.use(edgehandles);
-    } catch (e) {
-      console.log('failed', e);
-    }
+      cytoscape.use((edgehandles as unknown) as Ext);
+    } catch (e) {}
     return cytoscape;
   },
 });
